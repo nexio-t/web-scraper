6dfd7e24883002d0e307da7cc251e74522ba7dc7
Tomas Gear <tomas.e.gear@gmail.com>
2020-01-14 20:01:48 -0500

    added the mongoose connect lines

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.set("view engine", "handlebars");
 
 // app.use(routes);
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 app.listen(PORT, function() {
   console.log("Server listening on: http://localhost:" + PORT);
 });
